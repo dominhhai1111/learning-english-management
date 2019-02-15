aaa7bcb6f42a053ee7ba4b65c4e040983d4dcc4d
Hai Do Minh <hai.do.intern@ntq-solution.com.vn>
2019-02-15 17:04:22 +0700

    create-layout

--- resources/views/topics/list.blade.php
+++ resources/views/topics/list.blade.php
@@ -1,3 +1,18 @@
-@foreach ($topics as $topic)
-	<p>{{$topic['name']}}</p>
-@endforeach
+@extends('layout')
+
+@section('page-inner')
+	<div id="page-inner">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2>ADMIN DASHBOARD</h2>
+			</div>
+		</div>
+		<hr />
+		<div class="row">
+			@foreach ($topics as $topic)
+				<p style="margin: 0">{{$topic['name']}}</p>
+			@endforeach
+		</div>
+	</div>
+@endsection
+
