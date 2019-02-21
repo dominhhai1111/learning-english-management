22bdecfb144bb7d19e26c859bedbb1b0473dbdd3
Do Minh Hai <dominhhai1111@gmail.com>
2019-02-21 23:38:14 +0700

    build-adminlte

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,8 @@
 	<link href="../css/custom.css" rel="stylesheet" />
 	<!-- GOOGLE FONTS-->
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+	@yield('head-script')
 </head>
 <body>
 
---
+++ resources/views/topics/add.blade.php
@@ -0,0 +1,48 @@
+@extends('layout')
+
+@section('page-inner')
+    <div id="page-inner">
+        <div class="row">
+            <div class="title-page col-lg-12">
+                <h2>Add new topic</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="box box-primary col-lg-12">
+                <!-- form start -->
+                <form role="form">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputFile">File input</label>
+                            <input type="file" id="exampleInputFile">
+
+                            <p class="help-block">Example block-level help text here.</p>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Check me out
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('bottom-script')
+    <link rel="stylesheet" href="../AdminLTE/css/AdminLTE.min.css">
+@endsection
