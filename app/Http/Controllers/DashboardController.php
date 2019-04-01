07a9b4854e9bd03511f96b377fc420cdeefca4c2
Do Minh Hai <dominhhai1111@gmail.com>
2019-04-01 22:40:06 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        Mail::send('mail.test', ['test' => 'Test123'], function($message) {
+            $message->to('nomcbwcz@sharklasers.com')->subject('Test laravel mail');
+        });
+
         return view('dashboard');
     }
 }
